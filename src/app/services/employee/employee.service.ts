cba5b02081e902bbef5da3f0bd6a746525e9ddb4
unknown <m.rutkowski0909@gmail.com>
2022-11-21 15:48:20 +0100

    refactor

--- src/app/services/employee/employee.service.ts
+++ src/app/services/employee/employee.service.ts
@@ -2,14 +2,29 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
 import { EmployeeCreateModel } from 'src/app/model/employee-create.model';
+import { EmployeeResponse } from 'src/app/model/employee-response.model';
 import { PersonModel } from 'src/app/model/person.model';
+import { ApiResponse } from '../api.response';
 
 @Injectable()
 export class EmployeesService {
   constructor(private _http: HttpClient) {}
 
   getAll(): Observable<PersonModel[]> {
-    return this._http.get<PersonModel[]>('assets/data/people.json');
+    return this._http
+      .get<ApiResponse<EmployeeResponse[]>>(
+        'https://dummy.restapiexample.com/api/v1/employees'
+      )
+      .pipe(
+        map(
+          (resp) => <PersonModel[]>resp.data.map((data) => ({
+              personalNumber: data.id,
+              name: data.employee_name,
+              mail: data.employee_name + '@lowgular.io',
+              img: data.profile_image,
+            }))
+        )
+      );
   }
 
   create(employee: EmployeeCreateModel): Observable<void> {
